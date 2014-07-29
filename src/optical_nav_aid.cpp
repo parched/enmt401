d84eeffa3f6c29f03a07f0f2ef64abcac5cf9a70
James Duley <jagduley@gmail.com>
2014-07-29 16:54:21 +1200

    moved pose related functions into pose.ccp

--- src/optical_nav_aid.cpp
+++ src/optical_nav_aid.cpp
@@ -10,6 +10,7 @@
 #include <opencv2/nonfree/nonfree.hpp>
 
 #include "matches.hpp"
+#include "pose.hpp"
 
 /* This needs to go last or it causes an error with c++11 */
 #include <argp.h>
@@ -54,42 +55,6 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 }
 
 static struct argp argp = {options, parse_opt, 0, doc, 0, 0, 0};
-
-void getRt(Mat_<double> &E, Mat_<double> &R, Mat_<double> &t) {
-
-	// SVD to get projection
-	Mat w, u, vt;
-	SVD::compute(E, w, u, vt, SVD::MODIFY_A);
-
-	Matx33d W(0., -1., 0., 1., 0., 0., 0., 0., 1.);
-
-	R = u * Mat(W) * vt;
-	
-	t = u.col(2);
-}
-
-void getP(Matx34d &P, const Mat_<double> &R, const Mat_<double> &t) {
-	P = Matx34d(R(0, 0), R(0, 1), R(0, 2), t(0), 
-			R(1, 0), R(1, 1), R(1, 2), t(1), 
-			R(2, 0), R(2, 1), R(2, 2), t(2));
-}
-
-bool checkCoherentRotation(const Mat_<double> &R) {
-	if (std::abs(determinant(R)) - 1. > 1e-07) {
-		std::cerr << "|det(R)| != 1, not a rotation matrix" << std::endl;
-		return false;
-	}
-
-	return true;
-}
-
-void printEulerAngles(const Mat_<double> &R) {
-	double alpha = std::atan2(R(2, 1), R(2, 2));
-	double beta = std::atan2(-R(2, 0), sqrt(R(2, 1) * R(2, 1) + R(2, 2) * R(2, 2)));
-	double gamma = std::atan2(R(1, 0), R(0, 0));
-
-	std::cout << alpha << " " << beta << " " << gamma << " " << std::endl;
-}
 
 enum input_t {PIC, VID, CAM};
 
---
+++ src/pose.hpp
@@ -0,0 +1,47 @@
+/**
+ * \file pose.hpp
+ * \author James Duley
+ * \version 1.0
+ */
+
+#ifndef POSE_HPP
+#define POSE_HPP
+
+
+#include <opencv2/core/core.hpp>
+
+/**
+ * \brief Decomposes the essential matirx into the rotation and translation.
+ *
+ * \param E The essential matrix to decompose.
+ * \param R The rotation matrix.
+ * \param t The translation of unit scale.
+ */
+void getRt(const cv::Mat_<double> &E, cv::Mat_<double> &R, cv::Mat_<double> &t);
+
+/**
+ * \brief Composes the camera projection matrix.
+ *
+ * \param P The camera projection matrix.
+ * \param R The roation matrix.
+ * \param t The translation matrix.
+ */
+void getP(cv::Matx34d &P, const cv::Mat_<double> &R, const cv::Mat_<double> &t);
+
+/**
+ * \brief Check whether the rotation has unit determinant.
+ *
+ * \param R The rotation matrix.
+ *
+ * \return True if the matrix is coherent.
+ */
+bool checkCoherentRotation(const cv::Mat_<double> &R);
+
+/**
+ * \brief Print the Euler angles of a rotation matrix.
+ *
+ * \param R The rotation matrix.
+ */
+void printEulerAngles(const cv::Mat_<double> &R);
+
+#endif
